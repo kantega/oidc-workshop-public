39d844054b4f055cdded555af4cd222a8c8790b7
Oyvinkg <oyvind.grutle@kantega.no>
2023-09-05 09:47:07 +0200

    Add clientId and Secret for workshop Change Vipps documentation url

--- oidc-client/core/variables.py
+++ oidc-client/core/variables.py
@@ -7,5 +7,5 @@ RESPONSE_TYPE = 'code'
 GRANT_TYPE = 'authorization_code'
 REDIRECT_URL = 'http://localhost:8000/callback'
 
-CLIENT_ID = ''
-CLIENT_SECRET = ''
+CLIENT_ID = '970a5857-7796-42f9-96f2-cd7c54c23a54'
+CLIENT_SECRET = 'JZC8Q~Bkq1lVV1EuxnRmcLztwApYEGryMPvZkbBJ'
